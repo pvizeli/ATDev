07db0abb2b931ae470b404fdb9313e75bebedc69
pvizeli <pascal.vizeli@syshack.ch>
2015-12-05 09:30:18 +0100

    Weitere Detail arbeiten
    - Update AT manual SIM5218

--- src/SIM5218.cpp
+++ src/SIM5218.cpp
@@ -1,12 +1,82 @@
 
 #include<SIM5218.h>
-#include<Arduino.h>
 
-SIM5218_sms::clearUp()
+
+SIM5218_sms::cleanUp()
 {
-    memset(m_number, 0, SIM5218_BUFF_NUM_SIZE + 1);
-    memset(m_message, 0, SIM5218_BUFF_SMS_SIZE + 1);
+    memset(m_number, 0, SIM5218_NUM_SIZE + 1);
+    memset(m_message, 0, SIM5218_SMS_SIZE + 1);
 }
 
+
+SIM5218_gps::cleanUp()
+{
+    memset(m_latitude, 0, SIM5218_LATITUDE_SIZE + 1);
+    memset(m_longitude, 0, SIM5218_LONGITUDE_SIZE + 1);
+    memset(m_altitude, 0, SIM5218_ALTITUDE_SIZE + 1);
+    memset(m_speed, 0, SIM5218_SPEED_SIZE + 1);
+
+    m_latPos    = 0x00;
+    m_longPos   = 0x00;
+}
+
+SIM5218::SIM5218()
+{
+    memset(m_endBuffer, 0, SIM5218_BUFF_END_SIZE + 1);
+    memset(m_cmdBuffer, 0, SIM5218_BUFF_CMD_SIZE + 1);
+    memset(m_msgBuffer, 0, SIM5218_BUFF_MSG_SIZE + 1);
+
+    m_msgCount = 0;
+}
+
+SIM5218::~SIM5218()
+{
+
+}
+
+uint8_t SIM5218::sendATCmd(bool defaultEnd, uint16_t timeout)
+{
+    // init Buffer
+    memset(m_cmdBuffer, 0, SIM5218_BUFF_CMD_SIZE + 1);
+    memset(m_msgBuffer, 0, SIM5218_BUFF_MSG_SIZE + 1);
+
+    // init Counter
+    m_msgCount = 0;
+    
+    // is it the default AT end or had his own end?
+    if (defaultEnd) {
+        // set default end of AT communication
+        memset(m_endBuffer, 0, SIM5218_BUFF_END_SIZE + 1);
+        strncpy_P(m_endBuffer, SIM5218_END_OK, SIM5218_BUFF_END_SIZE);    
+    }
+
+    // Clear input Serial buffer
+    while (Serial.read() >= 0);
+
+    // Send AT command
+    Serial.println(m_cmdBuffer);
+
+    // wait until all data are send
+    Serial.flush();
+
+    // process answer
+    do {
+
+        // if data in serial input buffer
+        while (Serial.available()) {
+
+            // buffer is full
+            if (m_msgCount + 1 >= SIM5218_BUFF_MSG_SIZE) {
+                return SIM5218_ERR_BUFFER_FULL;
+            }
+
+            // read into buffer
+            m_msgBuffer[++m_msgCount] = Serial.read();
+        }
+
+    } while (); // timeout
+
+    return SIM5218_ERR_TIMEOUT;
+}
 
 # vim: set sts=4 sw=4 ts=4 et:
--- src/SIM5218.h
+++ src/SIM5218.h
@@ -2,6 +2,9 @@
 #ifndef SIM5218_H
 #define SIM5218_H
 
+// includes
+#include<Arduino.h>
+
 // module on pin
 #define SIM5218_ONPIN 2
 
@@ -10,8 +13,9 @@
 #define SIM5218_LF 0x0A
 #define SIM5218_END 0x1A
 
-// Commands
+// commands
 #define SIM5218_CMD_AT PSTR("AT")
+#define SIM5218_CMD_CPIN PSTR("AT+CPIN=%d")
 #define SIM5218_CMD_CREG PSTR("AT+CREG=?")
 #define SIM5218_CMD_CGPS PSTR("AT+CGPS=%d,%d")
 #define SIM5218_CMD_CGPSINFO PSTR("AT+CGPSINFO")
@@ -19,23 +23,40 @@
 #define SIM5218_CMD_CMGS PSTR("AT+CMGS=\"%s\"")
 #define SIM5218_CMD_CMGF PSTR("AT+CMGF=%d")
 
-// Option
+// option
 #define SIM5218_OPT_CMGF_TXT 0x01
+#define SIM5218_OPT_GPS_ON 0x01
+#define SIM5218_OPT_GPS_OFF 0x00
+#define SIM5218_OPT_GPS_STD 0x01
 
-// Buffer size
-// The real size is SIZE+1 for char buffer
+// possible ends of AT command
+#define SIM5218_END_OK PSTR("OK\x0D\x0A")
+#define SIM5218_END_ERROR PSTR("ERROR\x0D\x0A")
+
+// buffer size
+// the real size is SIZE+1 for char buffer
 #define SIM5218_BUFF_CMD_SIZE 63
 #define SIM5218_BUFF_MSG_SIZE 255
-#define SIM5218_BUFF_SMS_SIZE 255
-#define SIM5218_BUFF_NUM_SIZE 15
+#define SIM5218_BUFF_END_SIZE 15
 
-// Error codes
+// data size
+// the real size is SIZE+1 for char buffer
+#define SIM5218_SMS_SIZE 160
+#define SIM5218_NUM_SIZE 15
+#define SIM5218_LATITUDE_SIZE 15
+#define SIM5218_LONGITUDE_SIZE 15
+#define SIM5218_ALTITUDE_SIZE 15
+#define SIM5218_SPEED_SIZE 8
+
+// error codes
 #define SIM5218_ERR_BUFFER_FULL 0x01
 #define SIM5218_ERR_ERROR_RECEIVED 0x02
 #define SIM5218_ERR_UNEXPECTED_RESULT 0x03
 #define SIM5218_ERR_TIMEOUT 0x04
 #define SIM5218_ERR_NO_DATA 0x05
 #define SIM5218_ERR_UNEXPECTED_EOD 0x06
+#define SIM5218_ERR_GPS_CONNECT 0x07
+#define SIM5218_ERR_GPS_NOT_INIT 0x08
 
 // Time-Outs
 #define SIM5218_DEFAULT_AT_TIMEOUT 2500
@@ -52,15 +73,42 @@ class SIM5218_sms
 
     public:
         /** Phone number string */
-        char m_number[SIM5218_BUFF_NUM_SIZE + 1];
+        char m_number[SIM5218_NUM_SIZE + 1];
 
         /** SMS body string */
-        char m_message[SIM5218_BUFF_SMS_SIZE + 1];
+        char m_message[SIM5218_SMS_SIZE + 1];
 
         /**
          * Cleanup all buffers.
          *
          * Call this function to make this object ready for new message.
+         */
+        void cleanUp();
+}
+
+/**
+ * Object for store gps data
+ */
+class SIM5218_gps
+{
+    private:
+        SIM5218_gps() {
+            this->cleanUp();
+        }
+
+    public:
+        char m_latitude[SIM5218_LATITUDE_SIZE];
+        char m_longitude[SIM5218_LONGITUDE_SIZE];
+        char m_altitude[SIM5218_ALTITUDE_SIZE];
+        char m_speed[SIM5218_SPEED_SIZE];
+
+        char m_latPos;
+        char m_longPos;
+
+        /**
+         * Cleanup all buffers.
+         *
+         * Call this function to make this object ready for new gps data.
          */
         void cleanUp();
 }
@@ -75,12 +123,17 @@ class SIM5218
 
     private:
 
-        // Buffers
-        char m_CmdBuffer[SIM5218_BUFF_CMD_SIZE + 1];
-        char m_MsgBuffer[SIM5218_BUFF_MSG_SIZE + 1];
+        /** Buffer for prepare AT commands they will send */
+        char m_cmdBuffer[SIM5218_BUFF_CMD_SIZE + 1];
 
-        // Buffer counter
-        uint16_t m_MsgCount;
+        /** Answer buffer they fill it after send AT commands */
+        char m_msgBuffer[SIM5218_BUFF_MSG_SIZE + 1];
+
+        /** Buffer for handling end of AT request */
+        char m_endBuffer[SIM5218_BUFF_END_SIZE + 1];
+
+        /** AT CMD Buffer counter */
+        uint16_t m_msgCount;
 
 
         // status
@@ -90,14 +143,14 @@ class SIM5218
          *
          *
          */
-        uint8_t sendATCmd(uint16_t timeout);
+        uint8_t sendATCmd(bool defaultEnd, uint16_t timeout);
 
         /**
          *
          *
          */
         uint8_t sendATCmd() {
-            return this->sendATCmd(SIM5218_DEFAULT_AT_TIMEOUT);
+            return this->sendATCmd(true, SIM5218_DEFAULT_AT_TIMEOUT);
         }
 
     public:
@@ -105,11 +158,19 @@ class SIM5218
         /** SMS Object for receive/send sms */
         SIM5218_sms m_smsObj;
 
+        /** GPS Object for store gps data */
+        SIM5218_gps m_gpsObj;
+
         /**
          *
          *
          */
         bool isReady();
+
+        /**
+         *
+         */
+        uint8_t setPin();
 
         /**
          *
@@ -125,9 +186,19 @@ class SIM5218
 
         /**
          *
+         */
+        uint8_t updateGPS();
+
+        /**
+         *
          *
          */
         uint8_t isReg();
+
+        /**
+         *
+         */
+        uint8_t sendSMS();
 }
 
 #endif
